bc2f53b88ac0337ac4cb2b93087290530abeb43f
Rohit Karande <rohitkarande543@gmail.com>
2022-11-18 08:33:17 +0530

    Update marioChar.js

--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,13 +1,11 @@
-
 const mongoose = require('mongoose');
-const port = 3000
-const app = require('./app');
-// mongoose.connect('mongodb://localhost/testaroo', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
-// mongoose.connection.once('open', () =>{
-//     console.log('connection established')
-// }).on('connectionError',(err) =>{
-//     console.log(err);
-// })
+//  Your code goes here
+const marioSchema = new mongoose.Schema({
+    name: String,
+    weight: Number,
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+const marioModel = mongoose.model("mariochar", marioSchema);
+
+module.exports = marioModel;
